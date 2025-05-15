6f474522b5afb55822a08e86a4eff1250988c2ab
Lauraiglesiasperez <laura.iglesias.perez@estudiantat.upc.edu>
2025-05-15 12:25:42 +0200

    Create node.py

---
+++ node.py
@@ -0,0 +1,28 @@
+import math
+
+class Node:
+    def __init__(self, name, x, y):
+        self.name = name
+        self.x = float(x)
+        self.y = float(y)
+        self.neighbors = []
+
+    def __str__(self):
+        return f"Node({self.name}, {self.x}, {self.y})"
+
+    def distance(self, other):
+        dx = self.x - other.x
+        dy = self.y - other.y
+        return math.sqrt(dx**2 + dy**2)
+
+
+def AddNeighbor(n1, n2):
+    if n2 in n1.neighbors:
+        return False
+    n1.neighbors.append(n2)
+    return True
+
+def Distance(n1, n2):
+    dx = n1.x - n2.x
+    dy = n1.y - n2.y
+    return math.sqrt(dx**2 + dy**2)
